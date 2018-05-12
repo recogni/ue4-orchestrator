1b86471e3d8eb94babe0c39939f7dc00d9d025f4
Eugene Feinberg <eugene@recogni.com>
2018-05-11 18:26:40 -0700

    Build only target based on host machine type

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 echo "Set the environment variable UE4 in the console or in the system variables"
 echo "For example in my Linux machine, I use 'export UE4=/home/unreal/UnrealEngine/4.18'"
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+esac
+echo ${machine}
+
 plugin_file=`pwd`/UE4Orchestrator.uplugin
 package_folder=`pwd`/Plugins/UE4Orchestrator
-"${UE4}"/Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -plugin=${plugin_file} -package=${package_folder} -rocket -targetplatforms=Mac
+"${UE4}"/Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -plugin=${plugin_file} -package=${package_folder} -rocket -targetplatforms=${machine}
